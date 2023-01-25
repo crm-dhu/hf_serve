a06e15dcbbe2f731f219fc9537d1391923d40aa3
Donglin Hu <donglin.hu@salesforce.com>
2023-01-24 22:00:49 -0800

    fix unmask

--- fastapi/unmask.py
+++ fastapi/unmask.py
@@ -8,31 +8,33 @@ unmasker = pipeline('fill-mask', model='bert-base-uncased')
 
 app = FastAPI()
 
-
 class ModelInput(BaseModel):
     input: str
 
-
-class ModelOutput(BaseModel):
+class ModelPrediction(BaseModel):
     score: float
     token: int
     token_str: str
     sequence: str
 
+class ModelOutput(BaseModel):
+    input: str
+    output: List[ModelPrediction]
 
 @app.get("/")
 async def hello():
     return "use endpoint /unmask and provide a masked_str with [MASK] filling the token being masked."
 
 @app.post("/unmask")
-async def unmask(payload: ModelInput) -> List[ModelOutput]:
+async def unmask(payload: ModelInput) -> ModelOutput:
     masked_str = payload.input
-    return unmasked(masked_str)
+    unmasked_res = unmasked(masked_str)
+    return ModelOutput(input=masked_str, output=unmasked_res) 
 
-def unmasked(x: str) -> List[ModelOutput]:
+def unmasked(x: str) -> List[ModelPrediction]:
     y = unmasker(x)
     y.sort(key=lambda i: i['score'], reverse=True)
-    return [ModelOutput.parse_obj(i) for i in y]
+    return [ModelPrediction.parse_obj(i) for i in y]
 
 @app.on_event("startup")
 async def init():
